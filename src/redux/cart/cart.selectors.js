47736d5ca3cabfa0382ba20a19848b2f7dd8496d
himanshu mandwal <hkushwah8@gmail.com>
2021-03-14 15:42:31 +0530

    updating our application with reselect, replaced cart selector with memization

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+//here library named as reselect is used which avoids the unnessary rendering
+import { createSelector } from 'reselect';
+//two types of selector inputSelector and outputSelector
+
+
+//input selector
+
+const selectCart = (state) => state.cart ;
+
+export const selectCartItems =  createSelector(
+  [selectCart], // first argument is an array of input selector
+  (cart) =>  cart.cartItems//this will return the value that we want out of this selector
+); //this is memoized selector
+
+export const selectCartItemCount = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((accumulatorQuantity, cartItem) => accumulatorQuantity+cartItem.quantity, 0),
+) //memoized count selector
