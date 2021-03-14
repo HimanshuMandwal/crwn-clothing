44fd39735e07ce48b04e065d249c36f86fd7a246
himanshu mandwal <hkushwah8@gmail.com>
2021-03-14 18:06:03 +0530

    added the feature of clear item from the checkoutPage

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,10 +1,15 @@
 import { cartActionType } from './cart.types';
 
 export const toggleCartHidden = () => ({  //this action is used to toggle the cart hidden so there is no need of payload as it is just going to false => true and vice versa
-    type : cartActionType.TOGGLE_CART_HIDDEN,
+  type: cartActionType.TOGGLE_CART_HIDDEN,
 })
 
 export const addItem = (item) => ({
-  type : cartActionType.ADD_ITEM,
+  type: cartActionType.ADD_ITEM,
   payload: item,
-} )
+});
+
+export const clearItemFromCart = (item) => ({
+  type: cartActionType.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,3 +1,4 @@
+import { act } from '@testing-library/react';
 import { cartActionType } from './cart.types';
 import { addItemToCart } from './cart.utils'
 
@@ -17,6 +18,11 @@ const cartReducer = ( state = INITIAL_STATE , action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems , action.payload),
+      }
+    case (cartActionType.CLEAR_ITEM_FROM_CART):
+      return {
+        ...state,
+        cartItems: state.cartItems.filter( cartItem => cartItem.id != action.payload.id  ) //this will keep the items which returns true
       }
     default:
       return state;
